f1abce2d358fbe3906747a28e9d46feedffa6a64
BangSeIn <60180830+BangSeIn@users.noreply.github.com>
2021-06-02 18:41:31 +0900

    Update hello.c
    ㄱㄱ

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-  printf("<h1>Hello, Sein! 6월 2일 소공 실습 Welcome!!!</h1>\n");
+  printf("<h1>Hello, Sein! 6월 2일 소공 실습 PM 6:41 수정 Welcome!!!</h1>\n");
   return 0;
 }
